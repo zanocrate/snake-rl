166ee05b841714f0a4627474f66bacf9b9b9c25a
Ubuntu <alessandro.zanoli@gmail.com>
2022-12-25 11:20:18 +0000

    Play script

--- play.py
+++ play.py
@@ -1,6 +1,8 @@
 from env import *
+from train import *
+import torch
 
-env = SnakeEnv('human',10,10)
+env = SnakeEnv(None,15,15)
 env.metadata["render_fps"] = 10 # default is 4
 action_to_string = {
     0:"UP",
@@ -13,7 +15,17 @@ terminated = False
 
 observation, info = env.reset()
 
+policy_net = torch.load('policy_net.pth')
+
 while not terminated:
-    action = env.action_space.sample()
+    
+    observation_reshaped = {
+        'screen' : observation['screen'].reshape(1,15,15),
+        'direction' : observation['direction'].reshape(1,4)
+    }
+
+    with torch.no_grad():
+        action = int(policy_net(observation_reshaped).argmax(-1).reshape(1,-1)[0][0])
+
     observation,reward,terminated,info = env.step(action)
     print(observation['screen'])
--- train.py
+++ train.py
@@ -278,43 +278,45 @@ if torch.cuda.is_available():
 else:
     num_episodes = 50000
 
-for i_episode in tqdm.trange(num_episodes):
-    # Initialize the environment and get it's state
-    state,_=env.reset()
+if __name__ == "__main__":
 
-    for t in count():
-        action = select_action(state)
-        observation, reward, terminated, _ = env.step(action.item())
-        reward = torch.tensor([reward], device=device)
-        done = terminated
+    for i_episode in tqdm.trange(num_episodes):
+        # Initialize the environment and get it's state
+        state,_=env.reset()
 
-        if terminated:
-            next_state = None
-        else:
-            next_state = observation
+        for t in count():
+            action = select_action(state)
+            observation, reward, terminated, _ = env.step(action.item())
+            reward = torch.tensor([reward], device=device)
+            done = terminated
 
-        # Store the transition in memory
-        memory.push(state, action, next_state, reward)
+            if terminated:
+                next_state = None
+            else:
+                next_state = observation
 
-        # Move to the next state
-        state = next_state
+            # Store the transition in memory
+            memory.push(state, action, next_state, reward)
 
-        # Perform one step of the optimization (on the policy network)
-        optimize_model()
+            # Move to the next state
+            state = next_state
 
-        # Soft update of the target network's weights
-        # θ′ ← τ θ + (1 −τ )θ′
-        target_net_state_dict = target_net.state_dict()
-        policy_net_state_dict = policy_net.state_dict()
-        for key in policy_net_state_dict:
-            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
-        target_net.load_state_dict(target_net_state_dict)
+            # Perform one step of the optimization (on the policy network)
+            optimize_model()
 
-        if done:
-            episode_durations.append(t + 1)
-            plot_durations()
-            break
+            # Soft update of the target network's weights
+            # θ′ ← τ θ + (1 −τ )θ′
+            target_net_state_dict = target_net.state_dict()
+            policy_net_state_dict = policy_net.state_dict()
+            for key in policy_net_state_dict:
+                target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+            target_net.load_state_dict(target_net_state_dict)
+
+            if done:
+                episode_durations.append(t + 1)
+                plot_durations()
+                break
 
 
-torch.save(policy_net,'policy_net.pth')
-torch.save(target_net,'target_net.pth')
+    torch.save(policy_net,'policy_net.pth')
+    torch.save(target_net,'target_net.pth')
